37793f9137c4c7960555570fc1b8bb387d3b3ba8
lukesnc <lukeasimone@gmail.com>
2021-12-28 02:02:52 -0500

    skeleton of main loop + rom thru argv

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
 #include "chip8.h"
 
-int main() {
-    Chip8 myChip8;
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: chip8 [ROM]" << std::endl;
+        return 1;
+    }
 
+    Chip8 myChip8;
     myChip8.init();
-    bool status = myChip8.load("../roms/test_opcode.ch8");
+
+    if (!myChip8.load(argv[1]))
+        return 1;
+
+    for (;;)
+    {
+        myChip8.emulate_cycle();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
+    }
 
     return 0;
 }
