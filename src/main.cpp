31cf727c442d396ef1e920714fbc1a79f7757bb3
lukesnc <lukeasimone@gmail.com>
2022-01-16 00:59:25 -0500

    Filled out main lil more

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
-#include <unistd.h>
-
 #include <SDL2/SDL.h>
+#include <chrono>
+#include <iostream>
+#include <thread>
 
 #include "chip8.h"
 
-#define CYCLE 16666
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 512
+#define CYCLE 16 // 60hz
+#define WIDTH 1024
+#define HEIGHT 512
 
 int main(int argc, char** argv)
 {
@@ -16,21 +16,27 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    Chip8 myChip8;
-    myChip8.init();
+    Chip8 myChip8 = Chip8();
 
     // Create window
-    SDL_Window* window = NULL;
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    window = SDL_CreateWindow("CHIP-8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == NULL) {
-        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    SDL_Window* window = SDL_CreateWindow("CHIP-8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
         return 1;
     }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_RenderSetLogicalSize(renderer, WIDTH, HEIGHT);
+
+    SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 64, 32);
+
+    unsigned int frame_buffer[64 * 32];
 
     // Load rom
     if (!myChip8.load(argv[1]))
@@ -40,7 +46,36 @@ int main(int argc, char** argv)
     for (;;) {
         myChip8.exec();
 
-        usleep(CYCLE);
+        // Check keyboard
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                std::cout << "Exiting..." << std::endl;
+                SDL_DestroyTexture(texture);
+                SDL_DestroyRenderer(renderer);
+                SDL_DestroyWindow(window);
+                SDL_Quit();
+                return 0;
+            }
+        }
+
+        // Update screen
+        if (myChip8.draw_flag) {
+            myChip8.draw_flag = false;
+
+            // Copy screen into frame buffer
+            for (int i = 0; i < 2048; i++)
+                frame_buffer[i] = (0x00FFFFFF * myChip8.gfx[i]) | 0xFF000000;
+
+            SDL_UpdateTexture(texture, NULL, frame_buffer, 64 * sizeof(unsigned int));
+
+            // Draw
+            SDL_RenderClear(renderer); // Clear screen
+            SDL_RenderCopy(renderer, texture, NULL, NULL);
+            SDL_RenderPresent(renderer);
+        }
+
+        std::this_thread::sleep_for(std::chrono::microseconds(CYCLE));
     }
 
     return 0;
